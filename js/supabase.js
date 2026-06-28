a7a8648062e6e203e1622da1f1e9517910fff727
Thor-2026 <strange98723@gmail.com>
2026-06-27 21:30:44 -0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -6,6 +6,7 @@ const supabase = supabaseClient.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );*/
+console.log("CORRECT supabase.js LOADED");
 const SUPABASE_URL = "https://ggohaurqbsyamznzuuaf.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ZpHvrV-a_BmznhfbVupvxQ_b5NKYPYk";
 
